526f2b7f36689e6be7f8d096a69390f2dbc65800
Sawyer <928331892@qq.com>
2024-06-26 12:32:53 +0800

    Update abblockfilters.py

--- abblockfilters.py
+++ abblockfilters.py
@@ -38,19 +38,15 @@ def CreatReadme(ruleList, fileName):
     
     with open(fileName, 'a') as f:
         f.write("# AdBlock DNS Filters\n")
-        f.write("适用于AdGuard的去广告规则，每8个小时更新一次。\n")
-        f.write("## 说明\n")
-        f.write("1. 定时从上游各规则源获取更新，合并去重。\n")
-        f.write("2. 使用两组国内、两组国外 DNS 服务，分别对上游各规则源拦截的域名进行解析，去除已无法解析的域名。（上游各规则源中存在大量已无法解析的域名，无需加入拦截规则）\n")
-        f.write("3. 本项目仅对上游规则进行合并、去重、去除无效域名，不做任何修改。如发现误拦截情况，可临时添加放行规则（如 `@@||www.example.com^$important`），并向上游规则反馈。\n\n")
+        f.write("适用于AdGuard的去广告规则，每8个小时更新一次\n")
         f.write("## 订阅链接\n")
-        f.write("1. AdGuard Home 等DNS拦截服务使用规则1\n")
+        f.write("1. AdGuardHome 等DNS拦截服务使用规则1\n")
         f.write("2. AdGuard 等浏览器插件使用规则1 + 规则2\n\n")
         f.write("| 规则 | 原始链接 | 加速链接 |\n")
         f.write("|:-|:-|:-|\n")
-        f.write("| 规则1：DNS 拦截 | [原始链接](https://raw.githubusercontent.com/C9LG/DNS-Blocklists/main/rules/adblockdns.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/C9LG/DNS-Blocklists/main/rules/adblockdns.txt) |\n")
+        f.write("| 规则1：DNS拦截 | [原始链接](https://raw.githubusercontent.com/C9LG/DNS-Blocklists/main/rules/adblockdns.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/C9LG/DNS-Blocklists/main/rules/adblockdns.txt) |\n")
         f.write("| 规则2：插件拦截 | [原始链接](https://raw.githubusercontent.com/C9LG/DNS-Blocklists/main/rules/adblockfilters.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/C9LG/DNS-Blocklists/main/rules/adblockfilters.txt) |\n")
-        f.write("## 上游规则源\n")
+        f.write("## 上游规则\n")
         f.write("感谢以下大佬们的辛苦付出！\n")
         f.write("\n")
         f.write("| 规则 | 类型 | 原始链接 | 加速链接 | 更新日期 |\n")
@@ -149,7 +145,7 @@ def CreatDNS(blockDict, unblockDict, fileName):
         os.remove(fileName)    
     with open(fileName, 'a') as f:
         f.write("! Title: DNS-Blocklists\n")
-        f.write("! Description: 适用于AdGuardHome的去广告规则。\n")
+        f.write("! Description: 适用于AdGuardHome的去广告规则\n")
         f.write("! Homepage: https://github.com/C9LG/DNS-Blocklists\n")
         f.write("! Source: https://raw.githubusercontent.com/C9LG/DNS-Blocklists/main/rules/adblockdns.txt\n")
         f.write("! Version: %s\n"%(time.strftime("%Y%m%d%H%M%S", time.localtime())))
@@ -176,7 +172,7 @@ def CreatFiter(filterList, fileName):
     
     with open(fileName, 'a') as f:
         f.write("! Title: Blocklists\n")
-        f.write("! Description: 适用于AdGuard的去广告规则。\n")
+        f.write("! Description: 适用于AdGuard的去广告规则\n")
         f.write("! Homepage: https://github.com/C9LG/DNS-Blocklists\n")
         f.write("! Source: https://raw.githubusercontent.com/C9LG/DNS-Blocklists/main/rules/adblockfilters.txt\n")
         f.write("! Version: %s\n"%(time.strftime("%Y%m%d%H%M%S", time.localtime())))
